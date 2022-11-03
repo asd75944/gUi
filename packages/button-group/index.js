f632caf9be8367254f372edbb01333379d9889eb
asd75944 <ke5205190@qq.com>
2022-11-03 14:39:31 +0800

    222

--- packages/button-group/index.js
+++
@@ -1,7 +0,0 @@
-import GuiButtonGroup from '../button/src/button-group';
-
-GuiButtonGroup.install = function(Vue) {
-  Vue.component(GuiButtonGroup.name, GuiButtonGroup)
-}
-
-export default GuiButtonGroup
